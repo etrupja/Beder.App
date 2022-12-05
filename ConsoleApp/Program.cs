ba2932c72555b806abdc4685c38ac39f50cc3e28
Ervis Trupja <ervis.trupja@projekt202.com>
2022-12-05 09:04:40 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿//string firstName = "Beder";
+//string lastName = "Uni";
+//int age = 20;
+
+
+//string GetFullName(string firstName, string lastName, int age)
+//{
+//    string fullNameWithAge = $"Emri i plote eshte: {firstName} {lastName} - {age}";
+//    return fullNameWithAge;
+//}
+
+//string existingStudent = GetFullName(firstName, lastName, age);
+//string firstStudentFullName = GetFullName("Anisa", "Hoxha", 20);
+//string secondStudentFullName = GetFullName("Erjeta", "Kamolli", 20);
+
+//Console.WriteLine("--------------------------");
+//Console.WriteLine(existingStudent);
+//Console.WriteLine(firstStudentFullName);
+//Console.WriteLine(secondStudentFullName);
+//Console.WriteLine("--------------------------");
+
+List<bool> booleanList = new List<bool>() { true, false, true };
+
+List<int> studentAges = new List<int>() { 20, 21, 20 };
+
+int[] studentAgesArray = new int[3];
+
+
+
+
